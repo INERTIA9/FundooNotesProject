342150437931d50ed91a37bca5b5ae844f8e7e4a
saurabh <dekapurwars@gmail.com>
2021-11-17 23:08:56 +0530

    added:forgot password path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ForgetpasswordComponent } from './Components/forgetpassword/forgetpassword.component';
 import { SigninComponent } from './Components/signin/signin.component';
-
 import { SignupComponent } from './Components/signup/signup.component';
 
 const routes: Routes = [
   {path:'signup', component:SignupComponent},
-  {path:'signin', component:SigninComponent}
+  {path:'signin', component:SigninComponent},
+  {path:'forgetpassword',component:ForgetpasswordComponent}
 ];
 
 @NgModule({
