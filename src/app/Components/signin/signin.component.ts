79904849fa029ab037a10e00dce7648abc1199f0
saurabh <dekapurwars@gmail.com>
2021-11-17 23:13:05 +0530

    added:validators for email and password

--- src/app/Components/signin/signin.component.ts
+++ src/app/Components/signin/signin.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserService } from 'src/app/services/userService/user.service';
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
   styleUrls: ['./signin.component.scss']
 })
 export class SigninComponent implements OnInit {
-
-  constructor() { }
+  loginForm!: FormGroup;
+  submitted = false;
+  constructor(private formBuilder: FormBuilder, private userService: UserService) { }
 
   ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    })
   }
+  onSubmit() {
 
+  }
 }
