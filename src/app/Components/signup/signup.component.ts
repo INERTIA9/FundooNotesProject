69ac6741cfe72c70757b800d354b1a46f0902a84
saurabh <dekapurwars@gmail.com>
2021-11-17 23:14:37 +0530

    added:validators

--- src/app/Components/signup/signup.component.ts
+++ src/app/Components/signup/signup.component.ts
@@ -1,35 +1,50 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
+import { UserService } from 'src/app/services/userService/user.service';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
-  signup:FormGroup;
-  submitted=false;
+  signupform: FormGroup;
+  submitted = false;
 
-  constructor(private formBuilder: FormBuilder) {
-    this.signup=this.formBuilder.group({
-      
-      email:['',[Validators.required, Validators.email]],
-      password:['',[Validators.required, Validators.minLength(6)]],
-      
+  constructor(private formBuilder: FormBuilder, private userService: UserService) {
+    this.signupform = this.formBuilder.group({
+      firstName: ['', [Validators.required]],
+      lastName: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      Confirm: ['', [Validators.required, Validators.minLength(6)]],
+      service:['advance']
+
     });
-   }
+  }
 
   ngOnInit(): void {
-  
+
   }
-onSubmit(){
-console.log(this.signup.value);
-  if(this.signup.valid){
-    console.log("valid")
-  }
-  else{
-    console.log("invalid");
+  onSubmit() {
+    console.log(this.signupform.value);
+    if (this.signupform.valid) {
+      let reqData={
+       service:this.signupform.value.service,
+        firstName:this.signupform.value.firstName,
+        lastName:this.signupform.value.lastName,
+        email:this.signupform.value.email,
+        password:this.signupform.value.password
+      }
+      console.log("valid");
+      this.userService.registration(reqData).subscribe((response: any) => {
+        console.log(response);
+      }, error => {
+        console.log(error);
+      });
+    }
+    else {
+      console.log("invalid");
+    }
   }
 }
-  }
 
