5205d7f4d7b4d73c9b3137dd453d4665344e9666
saurabh <dekapurwars@gmail.com>
2021-11-18 16:26:31 +0530

    added:modified forget password component

--- src/app/Components/forgetpassword/forgetpassword.component.ts
+++ src/app/Components/forgetpassword/forgetpassword.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from 'src/app/services/userService/user.service';
+
 
 @Component({
   selector: 'app-forgetpassword',
@@ -6,10 +10,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forgetpassword.component.scss']
 })
 export class ForgetpasswordComponent implements OnInit {
-
-  constructor() { }
+  forgotPasswordform!: FormGroup;
+  submitted = false;
+  token: any;
+  constructor(private formBuilder: FormBuilder, private userService: UserService, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.forgotPasswordform = this.formBuilder.group({
+      Confirm: ['', [Validators.required, Validators.minLength(6)]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+
+    this.token = this.activatedRoute.snapshot.paramMap.get('token');
+  }
+
+  onSubmit() {
+    console.log(this.forgotPasswordform.value);
+    if (this.forgotPasswordform.valid) {
+      let reqData = {
+        newPassword: this.forgotPasswordform.value.password,
+      }
+      console.log("data is in forgot password", reqData);
+      this.userService.resetPassword(reqData,this.token).subscribe((response: any) => console.log(response)
+      )
+
+    } else {
+      console.log("enter valid data");
+
+    }
+
   }
 
 }
