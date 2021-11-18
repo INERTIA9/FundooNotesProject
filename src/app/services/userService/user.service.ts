d4629da2b4fc0851a143db8dccc236dda842feac
saurabh <dekapurwars@gmail.com>
2021-11-18 16:29:35 +0530

    added:login method and data encoding

--- src/app/services/userService/user.service.ts
+++ src/app/services/userService/user.service.ts
@@ -1,12 +1,13 @@
-import { Token } from '@angular/compiler';
 import { Injectable } from '@angular/core';
 import { HttpService } from '../httpService/http.service';
 import { HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
   token: any
+  forgotPasswordform: any;
   constructor(private httpService: HttpService) {
     this.token = localStorage.getItem('token')
   }
@@ -20,10 +21,35 @@ export class UserService {
       })
 
     }
-
-
-
-
-   return this.httpService.postSevice('user/userSignUp', reqPayload, false, headers)
+    return this.httpService.postSevice('user/userSignUp', reqPayload, false, headers)
   }
+  login(reqData: any) {
+    let headers = {
+      headers: new HttpHeaders({
+        'Content-type': 'application/json',
+        Authorization: this.token
+      })
+    }
+    return this.httpService.postSevice('user/login', reqData, false, headers)
+  }
+  encode(reqData: any) {
+    const formBody = [];
+    for (const property in reqData) {
+      const encodedKey = encodeURIComponent(property);
+      const encodedValue = encodeURIComponent(reqData[property]);
+      formBody.push(encodedKey + '=' + encodedValue);
+    }
+    return formBody.join('&');
+  }
+  resetPassword(reqData: any, token: any) {
+    let headers = {
+      headers: new Headers({
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': this.token
+      })
+    }
+    return this.httpService.postSevice('user/reset-password', this.encode(reqData), true, headers)
+
+  }
+
 }
