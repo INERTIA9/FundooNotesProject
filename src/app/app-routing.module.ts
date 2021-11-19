d65abb2086fa8b67ed24719ce5fb33086f8b635c
saurabh <dekapurwars@gmail.com>
2021-11-19 22:52:00 +0530

    added:forget email path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ForgetemailComponent } from './Components/forgetemail/forgetemail.component';
 import { ForgetpasswordComponent } from './Components/forgetpassword/forgetpassword.component';
 import { SigninComponent } from './Components/signin/signin.component';
 import { SignupComponent } from './Components/signup/signup.component';
@@ -8,8 +9,9 @@ const routes: Routes = [
   
   {path:'signup', component:SignupComponent},
   {path:'signin', component:SigninComponent},
-  {path:'forgetpassword',component:ForgetpasswordComponent},
+  // {path:'forgetpassword',component:ForgetpasswordComponent},
   {path:'resetpassword/:token',component:ForgetpasswordComponent},
+  {path:'forgetemail',component:ForgetemailComponent},
 ];
 
 @NgModule({
