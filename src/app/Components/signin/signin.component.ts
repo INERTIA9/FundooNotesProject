5f31cba246b888d5569b5d46ef7eaedf17b81352
saurabh <dekapurwars@gmail.com>
2021-11-24 16:10:01 +0530

    imported:router outlet

--- src/app/Components/signin/signin.component.ts
+++ src/app/Components/signin/signin.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { UserService } from 'src/app/services/userService/user.service';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
@@ -9,7 +11,7 @@ import { UserService } from 'src/app/services/userService/user.service';
 export class SigninComponent implements OnInit {
   loginForm!: FormGroup;
   submitted = false;
-  constructor(private formBuilder: FormBuilder, private userService: UserService) { }
+  constructor(private formBuilder: FormBuilder, private userService: UserService, private router: Router) { }
 
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
@@ -29,12 +31,19 @@ export class SigninComponent implements OnInit {
 
       this.userService.login(reqData).subscribe((result: any) => {
         console.log(result);
+        localStorage.setItem('token', result.id)
+        this.router.navigateByUrl('/dashboard')
 
+      }, error => {
+        console.log(error);
       })
     } else {
       console.log("invalid");
 
     }
 
+  }
+  onforgetemail() {
+    this.router.navigateByUrl('/forgetemail')
   }
 }
