22116314a31a79a0a27faf15fdb8a568a80a542b
saurabh <dekapurwars@gmail.com>
2021-11-24 16:07:09 +0530

    added:child path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,31 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreatenotesComponent } from './Components/createnotes/createnotes.component';
+import { DashboardComponent } from './Components/dashboard/dashboard.component';
+import { DisplaycardsComponent } from './Components/displaycards/displaycards.component';
 import { ForgetemailComponent } from './Components/forgetemail/forgetemail.component';
 import { ForgetpasswordComponent } from './Components/forgetpassword/forgetpassword.component';
+import { GetallnotesComponent } from './Components/getallnotes/getallnotes.component';
+import { IconsComponent } from './Components/icons/icons.component';
 import { SigninComponent } from './Components/signin/signin.component';
 import { SignupComponent } from './Components/signup/signup.component';
 
 const routes: Routes = [
-  
-  {path:'signup', component:SignupComponent},
-  {path:'signin', component:SigninComponent},
-  // {path:'forgetpassword',component:ForgetpasswordComponent},
-  {path:'resetpassword/:token',component:ForgetpasswordComponent},
-  {path:'forgetemail',component:ForgetemailComponent},
+
+  { path: 'signup', component: SignupComponent },
+  { path: 'signin', component: SigninComponent },
+  { path: 'resetpassword/:token', component: ForgetpasswordComponent },
+  { path: 'forgetemail', component: ForgetemailComponent },
+  {
+    path: 'dashboard', component: DashboardComponent,
+    children: [
+
+      { path: 'notes', component: GetallnotesComponent }
+
+
+    ]
+  }
+
 ];
 
 @NgModule({
