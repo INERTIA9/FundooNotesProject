1c4dec5c91ea332fa4451bc5f6f4bd01abe5f957
saurabh <dekapurwars@gmail.com>
2021-11-25 23:03:25 +0530

    added:delete serrvice parameters

--- src/app/services/httpService/http.service.ts
+++ src/app/services/httpService/http.service.ts
@@ -8,20 +8,20 @@ export class HttpService {
   BaseUrl = environment.BaseUrl
   constructor(private httpClient: HttpClient) { }
 
-  postService(url:string = '', reqPayload:Object = {}, token: boolean = false, httpOptions:any = {}) {
+  postService(url: string = '', reqPayload: Object = {}, token: boolean = false, httpOptions: any = {}) {
     console.log(reqPayload);
-     return this.httpClient.post(this.BaseUrl + url, reqPayload, token && httpOptions)
+    return this.httpClient.post(this.BaseUrl + url, reqPayload, token && httpOptions)
 
   }
-  getSevice(url:string = '', token: boolean = false, httpOptions:any = null) {
-    
-    return this.httpClient.get(this.BaseUrl + url,token && httpOptions)
+  getSevice(url: string = '', token: boolean = false, httpOptions: any = null) {
+
+    return this.httpClient.get(this.BaseUrl + url, token && httpOptions)
   }
   putSevice() {
 
   }
-  deleteSevice() {
-
+  deleteSevice(url: string = '', token: boolean = false, httpOptions: any = null) {
+    return this.httpClient.get(this.BaseUrl + url, token && httpOptions)
   }
   patchSevice() {
 
