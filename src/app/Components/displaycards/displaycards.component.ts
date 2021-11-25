c267ee2796ab819cbe0863e1bdaee1b85fbfb14d
saurabh <dekapurwars@gmail.com>
2021-11-25 23:07:39 +0530

    added:event emitter

--- src/app/Components/displaycards/displaycards.component.ts
+++ src/app/Components/displaycards/displaycards.component.ts
@@ -1,7 +1,9 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { GetallnotesComponent } from '../getallnotes/getallnotes.component';
 import { IconsComponent } from '../icons/icons.component';
+import { UpdatenotesComponent } from '../updatenotes/updatenotes.component';
 
 
 @Component({
@@ -11,10 +13,11 @@ import { IconsComponent } from '../icons/icons.component';
 })
 export class DisplaycardsComponent implements OnInit {
   expanddiv: boolean = false
-  note: any;
+  sentmsg: any
   constructor(public dialog: MatDialog) { }
 
   @Input() notesarray: any
+  @Output() displaytogetallnotes = new EventEmitter<string>();
   ngOnInit(): void {
 
   }
@@ -22,20 +25,24 @@ export class DisplaycardsComponent implements OnInit {
     this.expanddiv = true;
   }
   openDialog(note: any) {
-    const dialogRef = this.dialog.open(GetallnotesComponent,{
-      width:"600px",
-      height:"300px",
-      
-     
-    } );
-    dialogRef.componentInstance.note= note.title;
-    dialogRef.componentInstance.note = note.description;
-     
+    const dialogRef = this.dialog.open(UpdatenotesComponent, {
+      width: "600px",
+      data: note
+
+
+    });
+
+
     dialogRef.afterClosed().subscribe(result => {
       console.log(`Dialog result: ${result}`);
     });
   }
+  recievefromiconstodisplaycard($event: any) {
+    console.log("recievedindisplay",$event);
+    this.sentmsg = $event
+    this.displaytogetallnotes.emit(this.sentmsg)
 
+  }
 }
 
 
