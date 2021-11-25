6642e71bdff49e12e6e40b558c5f95617331a4c5
saurabh <dekapurwars@gmail.com>
2021-11-25 23:09:06 +0530

    removed:unwanted typescript

--- src/app/Components/dashboard/dashboard.component.ts
+++ src/app/Components/dashboard/dashboard.component.ts
@@ -16,17 +16,8 @@ export class DashboardComponent implements OnDestroy {
   mobileQuery: MediaQueryList;
   panelOpenState = false;
   fillerNav = Array.from({ length: 50 }, (_, i) => `Nav Item ${i + 1}`);
-
-  // fillerContent = Array.from(
-  //   {length: 50},
-  //   () =>
-  //     `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-  //      labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-  //      laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-  //      voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-  //      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
-  // );
-
+  token: any
+ 
   private _mobileQueryListener: () => void;
 
   constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher, private router: Router) {
@@ -39,11 +30,22 @@ export class DashboardComponent implements OnDestroy {
     this.mobileQuery.removeListener(this._mobileQueryListener);
   }
   onnote() {
-    console.log("hiii");
-
-
+    console.log("onnotemethod");
     this.router.navigateByUrl('/dashboard/notes')
 
   }
+  logout() {
+    localStorage.removeItem('token');
+    this.router.navigateByUrl('/signin');
+  }
 
+  ontrash(){
+    console.log("ontrashmethod");
+    this.router.navigateByUrl('/dashboard/gettrash')
+  }
+  onarchive(){
+    console.log("onarchive");
+    this.router.navigateByUrl('/dashboard/getarchive')
+    
+  }
 }
