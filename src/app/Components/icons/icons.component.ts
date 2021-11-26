257f4b679f8ea73d8e429695a63b0e380e45440d
saurabh <dekapurwars@gmail.com>
2021-11-26 19:19:59 +0530

    added:itrashed component

--- src/app/Components/icons/icons.component.ts
+++ src/app/Components/icons/icons.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { NoteserviceService } from 'src/app/services/noteservice/noteservice.service';
-
+import { ActivatedRoute } from '@angular/router';
+import { GettrashlistComponent } from '../gettrashlist/gettrashlist.component';
 @Component({
   selector: 'app-icons',
   templateUrl: './icons.component.html',
@@ -9,11 +10,15 @@ import { NoteserviceService } from 'src/app/services/noteservice/noteservice.ser
 export class IconsComponent implements OnInit {
   @Input() noteCard: any
   @Output() iconstodisplay = new EventEmitter<string>();
-  constructor(private noteservices: NoteserviceService) { }
-
+  constructor(private noteservices: NoteserviceService,private activatedroute:ActivatedRoute) { }
+  isTrashed:boolean=false
   ngOnInit(): void {
-    // console.log(this.noteCard.id);
+    let com = this.activatedroute.snapshot.component;
 
+    if (com == GettrashlistComponent) {
+      this.isTrashed = true;
+      console.log(this.isTrashed);
+    }
   }
   ondelete() {
     // console.log(this.noteCard.id);
@@ -29,14 +34,29 @@ export class IconsComponent implements OnInit {
     })
 
   }
-  onarchive(){
+  onarchive() {
     let req = {
       noteIdList: [this.noteCard.id],
       isArchived: true,
     }
-    this.noteservices.archivedservice(req).subscribe((res:any)=>{
+    this.noteservices.archivedservice(req).subscribe((res: any) => {
       console.log((res.data));
       this.iconstodisplay.emit(res)
+    })
+  }
+
+
+  ondeleteforever() {
+    let req = {
+      noteIdlist: [this.noteCard.id],
+      isDeleted: true,
+    }
+    this.noteservices.deleteforever(req).subscribe((res: any) => {
+      console.log("in icon");
+      
+      console.log(res.data);
+      // this.iconstodisplay.emit(res)
+
     })
   }
 }
