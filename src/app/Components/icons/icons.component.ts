c557cd040f7e5c1736c71d9bc54b8e13f69c2f8a
saurabh <dekapurwars@gmail.com>
2021-11-29 08:09:38 +0530

    added: variable forgetarcchived component

--- src/app/Components/icons/icons.component.ts
+++ src/app/Components/icons/icons.component.ts
@@ -2,6 +2,9 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { NoteserviceService } from 'src/app/services/noteservice/noteservice.service';
 import { ActivatedRoute } from '@angular/router';
 import { GettrashlistComponent } from '../gettrashlist/gettrashlist.component';
+import { GetarchivedlistComponent } from '../getarchivedlist/getarchivedlist.component';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+
 @Component({
   selector: 'app-icons',
   templateUrl: './icons.component.html',
@@ -10,8 +13,11 @@ import { GettrashlistComponent } from '../gettrashlist/gettrashlist.component';
 export class IconsComponent implements OnInit {
   @Input() noteCard: any
   @Output() iconstodisplay = new EventEmitter<string>();
-  constructor(private noteservices: NoteserviceService,private activatedroute:ActivatedRoute) { }
-  isTrashed:boolean=false
+  constructor(private noteservices: NoteserviceService, private activatedroute: ActivatedRoute, private _snackBar: MatSnackBar) { }
+  isTrashed: boolean = false
+  isArchived: boolean = false
+  colorarray = ['#F44336', 'red', 'white', 'blue', 'black'];
+
   ngOnInit(): void {
     let com = this.activatedroute.snapshot.component;
 
@@ -19,6 +25,16 @@ export class IconsComponent implements OnInit {
       this.isTrashed = true;
       console.log(this.isTrashed);
     }
+    let som = this.activatedroute.snapshot.component;
+
+    if (som == GetarchivedlistComponent) {
+      this.isArchived = true;
+      console.log(this.isTrashed);
+    }
+
+
+
+
   }
   ondelete() {
     // console.log(this.noteCard.id);
@@ -29,8 +45,14 @@ export class IconsComponent implements OnInit {
     }
     this.noteservices.trashnoteservice(req).subscribe((res: any) => {
       console.log("inside icon calling trash ", res.data);
-
+      this._snackBar.open("Note Trashed", 'Undo', {
+        horizontalPosition: 'start',
+        duration: 2000,
+      })
       this.iconstodisplay.emit(res)
+    }, error => {
+      console.log(error);
+
     })
 
   }
@@ -41,21 +63,78 @@ export class IconsComponent implements OnInit {
     }
     this.noteservices.archivedservice(req).subscribe((res: any) => {
       console.log((res.data));
+      this._snackBar.open("Note Archived", 'Undo', {
+        horizontalPosition: 'start',
+        duration: 2000,
+      })
       this.iconstodisplay.emit(res)
+    }, error => {
+      console.log(error);
+
+    })
+  }
+  ondeleteforever() {
+    let req = {
+      noteIdList: [this.noteCard.id],
+      isDeleted: false,
+    }
+    this.noteservices.deleteforever(req).subscribe((res: any) => {
+      console.log((res.data));
+      this._snackBar.open("Note deleted", 'Undo', {
+        duration: 2000,
+      })
+      this.iconstodisplay.emit(res)
+    }, error => {
+      console.log(error);
+
     })
   }
 
 
-  ondeleteforever() {
+
+changecolor(data: any) {
     let req = {
       noteIdlist: [this.noteCard.id],
-      isDeleted: true,
+      color: data
     }
-    this.noteservices.deleteforever(req).subscribe((res: any) => {
+    this.noteservices.changecolorservice(req).subscribe((res: any) => {
       console.log("in icon");
-      
       console.log(res.data);
-      // this.iconstodisplay.emit(res)
+      this.iconstodisplay.emit(res)
+
+    })
+  }
+  onunarchive() {
+    let req = {
+      noteIdList: [this.noteCard.id],
+      isArchived: false,
+    }
+    this.noteservices.archivedservice(req).subscribe((res: any) => {
+      console.log((res.data));
+      this._snackBar.open("Note Archived", 'Undo', {
+        horizontalPosition: 'start',
+        duration: 2000,
+      })
+      this.iconstodisplay.emit(res)
+    }, error => {
+      console.log(error);
+
+    })
+  }
+  onRestore() {
+    let req = {
+      noteIdList: [this.noteCard.id],
+      isDeleted: false,
+    }
+    this.noteservices.trashnoteservice(req).subscribe((res: any) => {
+      console.log((res.data));
+      this._snackBar.open("Note Restored", 'Undo', {
+        horizontalPosition: 'start',
+        duration: 2000,
+      })
+      this.iconstodisplay.emit(res)
+    }, error => {
+      console.log(error);
 
     })
   }
