55520851401b4ce3b78d8502c8f7972fd763991e
saurabh <dekapurwars@gmail.com>
2021-12-06 08:14:37 +0530

    Added:imported data services component

--- src/app/Components/dashboard/dashboard.component.ts
+++ src/app/Components/dashboard/dashboard.component.ts
@@ -4,7 +4,8 @@ import { ChangeDetectorRef, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { style } from '@angular/animations';
 import { MatButton } from '@angular/material/button';
-import {MatSnackBar, MatSnackBarModule} from '@angular/material/snack-bar';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { DataService } from 'src/app/services/dataservice/data.service';
 
 
 @Component({
@@ -18,11 +19,12 @@ export class DashboardComponent implements OnDestroy {
   panelOpenState = false;
   fillerNav = Array.from({ length: 50 }, (_, i) => `Nav Item ${i + 1}`);
   token: any
+  searchword: any
 
- 
+
   private _mobileQueryListener: () => void;
 
-  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher, private router: Router,private _snackBar: MatSnackBar) {
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher, private router: Router, private _snackBar: MatSnackBar, private dataservice: DataService) {
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addListener(this._mobileQueryListener);
@@ -38,19 +40,33 @@ export class DashboardComponent implements OnDestroy {
   }
   logout() {
     localStorage.removeItem('token');
-    this._snackBar.open("You have been Logged Out",'',{
-      duration:2000,
+    this._snackBar.open("You have been Logged Out", '', {
+      duration: 2000,
     })
     this.router.navigateByUrl('/signin');
   }
 
-  ontrash(){
+  ontrash() {
     console.log("ontrashmethod");
     this.router.navigateByUrl('/dashboard/gettrash')
   }
-  onarchive(){
+  onarchive() {
     console.log("onarchive");
     this.router.navigateByUrl('/dashboard/getarchive')
-    
+
+  }
+  onreminder() {
+    console.log("onreminders");
+    this.router.navigateByUrl('/dashboard/reminders')
+
+  }
+  searchtitle(event: any) {
+    console.log(event.target.value);
+    this.value = event.target.value
+    let Ddata = {
+      type: 'search',
+      data: [this.value]
+    }
+    this.dataservice.changeData(Ddata)
   }
 }
