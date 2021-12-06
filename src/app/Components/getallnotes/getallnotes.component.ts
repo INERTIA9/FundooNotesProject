04c6515caa0b8921fd01c99d5a347ab15fac68fe
saurabh <dekapurwars@gmail.com>
2021-12-06 08:12:01 +0530

    added:Subscription for data service

--- src/app/Components/getallnotes/getallnotes.component.ts
+++ src/app/Components/getallnotes/getallnotes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NoteserviceService } from 'src/app/services/noteservice/noteservice.service';
+import { DataService } from 'src/app/services/dataservice/data.service';
 @Component({
   selector: 'app-getallnotes',
   templateUrl: './getallnotes.component.html',
@@ -9,12 +10,21 @@ export class GetallnotesComponent implements OnInit {
   Notelist: any;
   note: any;
   notedata: any
+  subscription: any;
+  message: any;
+  finaldateinfo: any;
 
-  constructor(private noteservice: NoteserviceService) { }
+
+  constructor(private noteservice: NoteserviceService, private dataservice: DataService) { }
 
   ngOnInit(): void {
     this.getallnotes()
+    this.subscription = this.dataservice.currentMessage.subscribe(message => {
+      this.message = message;
+      this.getallnotes();
+    });
   }
+
   getallnotes() {
     let notes = []
     this.noteservice.getallnotesservice().subscribe((result: any) => {
@@ -22,7 +32,7 @@ export class GetallnotesComponent implements OnInit {
       this.Notelist = result.data.data
       this.Notelist.reverse()
       this.Notelist = this.Notelist.filter((notedata: any) => {
-        return notedata.isDeleted === false && notedata.isArchived===false;
+        return notedata.isDeleted === false && notedata.isArchived === false;
 
       })
 
@@ -40,8 +50,13 @@ export class GetallnotesComponent implements OnInit {
     // this.message = $event
   }
   receiveMessagefromdisplaycard($event: any) {
-    console.log("insidegetallnotes",$event);
+    console.log("insidegetallnotes", $event);
     this.getallnotes()
+  }
+  recievedateinfofromdisplaycard($event: any){
+    console.log("dateinfofromdisplaytogetallnotes",$event);
+    this.finaldateinfo = $event
+    
   }
 }
 
